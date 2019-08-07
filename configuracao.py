64eb693d8d0b6335d21f8a1b524c9287b148eca7
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-08-07 13:47:32 -0300

    Calibração OpenCV

--- configuracao.py
+++ configuracao.py
@@ -12,32 +12,32 @@ objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 # Arrays para armazenar pontos de objeto e pontos de imagem de todas as imagens
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = [] # 3d ponto no espaço do mundo real
+imgpoints = [] # 2d pontos no plano da imagem.
 
 images = glob.glob('*.jpg')
 
 for fname in images:
 
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.imread(fname) #Carrega uma imagem de um arquivo
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Converte uma imagem de um espaço de cor para outro.
 
-    # Find the chess board corners
+    # Encontre os cantos do tabuleiro de xadrez
     ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
 
-    # If found, add object points, image points (after refining them)
+     # Se encontrado, adicione pontos de objeto, pontos de imagem (depois de refiná-los)
     if ret == True:
         objpoints.append(objp)
 
-        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria) #Refina os locais dos cantos.
         imgpoints.append(corners2)
 
-        # Draw and display the corners
+         # Desenhe e exiba os cantos
         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+        cv2.imshow('img',img) #Exibe textura OpenGL 2D na janela especificada.
+        cv2.waitKey(500) #Aguarda por uma tecla pressionada.
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() #Destrói todas as janelas do HighGUI.
 
 
 #--------------------------------------Calibração---------------------------------------------------
@@ -47,37 +47,38 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 
 #-------------------------------------Undistortion--------------------------------------------------
 
-img = cv2.imread('/home/joalison/Documentos/woorkspace/imagem20.jpg')
+img = cv2.imread('/home/joalison/Documentos/woorkspace/imagem20.jpg') #Carrega uma imagem de um arquivo.
 print(img)
 h,  w = img.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h)) #Retorna a nova matriz da câmera com base no parâmetro de escala livre.
 
 #----------------------------------Usadondo o unsdistort-------------------------------------------- 
 
 # undistort
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx) #Transforma uma imagem para compensar a distorção da lente.
 
 # crop the image
 x,y,w,h = roi
 dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult.png',dst)
+cv2.imwrite('calibresult.png',dst) #Salva uma imagem em um arquivo especificado.
 
 #----------------------------------Usando o remapeamento--------------------------------------------
 
 # undistort
 mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist, None, newcameramtx, (w,h), 5)
-dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
+dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR) #Aplica uma transformação geométrica genérica a uma imagem.
 
 # crop the image
 x,y,w,h = roi
 dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult.png',dst)
+cv2.imwrite('calibresult.png',dst) #Salva uma imagem em um arquivo especificado.
 
 #----------------------------------Erro de Re-projeção------------------------------------------------
 
 mean_error = 0
 for i in list(range(len(objpoints))):
-    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist) #Projetos 3D aponta para um plano de imagem.
+
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2) #Projetos 3D aponta para um plano de imagem.
     mean_error += error
 print( "total error: {}".format(mean_error/len(objpoints)) )
